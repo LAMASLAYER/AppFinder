821f854d54bbeb2c7caa3412ce46cea2bcb74512
ku714 <jerome.wilmart@clearstream.com>
2018-11-07 13:59:31 +0100

    fix for sql dialect + fix for server url

--- src/main/java/com/appfinder/utils/Utils.java
+++ src/main/java/com/appfinder/utils/Utils.java
@@ -1,7 +1,7 @@
 package com.appfinder.utils;
 
 public class Utils {
-    public static final String SERVER = "http://localhost:8080";
+    public static final String SERVER = "https://pathfinderappfinder.herokuapp.com";
 
     private Utils() {
     }
