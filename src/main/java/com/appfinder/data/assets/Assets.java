264e96aaa7b567b6868f3b0141b3a56b650936e2
ku714 <jerome.wilmart@clearstream.com>
2019-03-29 11:42:27 +0100

    width height for assets

--- src/main/java/com/appfinder/data/assets/Assets.java
+++ src/main/java/com/appfinder/data/assets/Assets.java
@@ -14,6 +14,8 @@ public class Assets {
     private int assetId;
     private String url;
     private String category;
+    private int width;
+    private int height;
 
 
     public int getAssetId() {
@@ -38,5 +40,21 @@ public class Assets {
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
